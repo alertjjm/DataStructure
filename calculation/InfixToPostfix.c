5dd8305d4c077b629a3fda0e61eeb9517ccb79c0
alertjjm <sdr2936@gmail.com>
2020-01-15 19:53:44 +0900

    200115

---
+++ calculation/InfixToPostfix.c
@@ -0,0 +1,132 @@
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "ListBaseStack.h"
+
+int GetOpPrec(char op)
+{
+	switch(op)
+	{
+	case '*':
+	case '/':
+		return 5;
+	case '+':
+	case '-':
+		return 3;
+	case '(':
+		return 1;
+	}
+
+	return -1;   // 등록되지 않은 연산자
+}
+
+int WhoPrecOp(char op1, char op2)
+{
+	int op1Prec = GetOpPrec(op1);
+	int op2Prec = GetOpPrec(op2);
+
+	if(op1Prec > op2Prec)
+		return 1;
+	else if(op1Prec < op2Prec)
+		return -1;
+	else
+		return 0;
+}
+
+void ConvToRPNExp(char exp[])
+{
+	Stack stack;
+	int expLen = strlen(exp);
+	char * convExp = (char*)malloc(expLen+1);
+
+	int i, idx=0;
+	char tok, popOp;
+	
+	memset(convExp, 0, sizeof(char)*expLen+1);
+	StackInit(&stack);
+
+	for(i=0; i<expLen; i++)
+	{
+		tok = exp[i];
+		if(isdigit(tok))
+		{
+			convExp[idx++] = tok;
+		}
+		else
+		{
+			switch(tok)
+			{
+			case '(':
+				SPush(&stack, tok);
+				break;
+
+			case ')':
+				while(1)
+				{
+					popOp = SPop(&stack);
+					if(popOp == '(')
+						break;
+					convExp[idx++] = popOp;
+				}
+				break;
+
+			case '+': case '-': 
+			case '*': case '/':
+				while(!SIsEmpty(&stack) && 
+						WhoPrecOp(SPeek(&stack), tok) >= 0)
+					convExp[idx++] = SPop(&stack);
+
+				SPush(&stack, tok);
+				break;
+			}
+		}
+	}
+
+	while(!SIsEmpty(&stack))
+		convExp[idx++] = SPop(&stack);
+
+	strcpy(exp, convExp);
+	free(convExp);
+}
+	int EvalRPNExp(char exp[]) {
+		Stack stack;
+		StackInit(&stack);
+
+		int len = strlen(exp);
+		int result;
+		int op1, op2;
+		for (int i = 0; i < len; i++) {
+			char t = exp[i];
+			if (isdigit(t)) {
+				SPush(&stack, t);
+			}
+			else {
+				op1 = SPop(&stack) - '0';
+				op2 = SPop(&stack) - '0';
+				switch (t) {
+				case '*':
+					result = op1*op2;
+					SPush(&stack, result + '0');
+					break;
+				case '/':
+					result = op2 / op1;
+					SPush(&stack, result + '0');
+					break;
+				case '+':
+					result = op1 + op2;
+					SPush(&stack, result + '0');
+					break;
+				case '-':
+					result = op2 - op1;
+					SPush(&stack, result + '0');
+					break;
+				}
+			}
+		}
+		while (!SIsEmpty(&stack))
+		{
+			Data d = SPop(&stack);
+		}
+
+		return result;
+	}
